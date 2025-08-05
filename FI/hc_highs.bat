232ae0afc67959a2657f043e1c78ddde42f01681
joaoflavioufmg <joao.flavio@dep.ufmg.br>
2025-08-04 21:32:55 -0300

    BR FR and FI

--- FI/hc_highs.bat
+++ FI/hc_highs.bat
@@ -1,15 +1,15 @@
 @echo off
     REM Batch file for running optimization solvers
-    REM State code: 0
+    REM State code: 1
 
     echo Running GLPSOL to generate MPS file...
-    glpsol -m hc_glpk.mod -d hc.dat -d 0_dados.dat -d 0_dist_dur.txt --wmps hc_glpk.mps --check
+    glpsol -m hc_glpk.mod -d hc.dat -d 1_dados.dat -d 1_dist_dur.txt --wmps hc_glpk.mps --check
 
     echo Running HiGHS solver...
     highs --model_file hc_glpk.mps --options_file hc_highs.opt
 
     echo Running GLPSOL with HiGHS solution...
-    glpsol -m hc_glpk.mod -d hc.dat -d 0_dados.dat -d 0_dist_dur.txt -r hc_highs.sol
+    glpsol -m hc_glpk.mod -d hc.dat -d 1_dados.dat -d 1_dist_dur.txt -r hc_highs.sol
 
     echo All solver commands completed.
     pause
